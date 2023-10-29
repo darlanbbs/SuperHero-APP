57830e7b13c87b8803c5fc004c1f35d11d8c905c
Darlan Bomfim <darlanbs05@gmail.com>
2023-10-29 10:15:55 -0300

    Fix:retirada do botao de search e estilizando componente de input

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -17,9 +17,6 @@ const Header = ({ value, onChange }: InputProps) => {
           onChange={onChange}
           placeholder="Type name hero"
         />
-        <button className="ml-2 px-4 py-2 bg-blue-600 text-white rounded-md">
-          Search
-        </button>
       </div>
     </div>
   );
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import TextField from "@mui/material/TextField";
 
 interface InputProps {
   value: string;
@@ -9,11 +8,9 @@ interface InputProps {
 
 const InputComponent = ({ value, onChange, placeholder }: InputProps) => {
   return (
-    <TextField
-      variant="filled"
-      color="primary"
-      focused
-      fullWidth
+    <input
+      type="text"
+      className="border border-gray-300 rounded-md p-2 w-full focus:outline-none focus:border-blue-500 decoration-black text-stone-950"
       value={value}
       onChange={onChange}
       placeholder={placeholder}
